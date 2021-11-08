53feb34fe203d096998b7fc5b84c333c56233fa0
NaranLidzhiev <lidzhiev-03@mail.ru>
2021-11-08 23:54:19 +0300

    присваивние элементов массива другому массиву

--- main.cpp
+++ main.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
 using namespace std;
 #include <stdlib.h>
-const int MAX = 5;
+#include <string.h>
+const int MAX = 11;
 int main() {
-    int field[MAX], dop, help;
-    int i, j, k;
-    int count = 0;
-    srand(56);
-    for (i = 0; i < MAX; i++) {
-        field[i] = rand() % 100 + 1;
-        cout << field[i] << " ";
-    }
-    cout << endl;
-    for (i = MAX-1; i > 0; i--) {
-        for (j = 0; j < i; j++) {
-            cout << "(" << j << "-" << j + 1 << "):";
-            if (field[j] > field[j + 1]) {
-                help = field[j];
-                field[j] = field[j + 1];
-                field[j + 1] = help;
-                count ++;
-            }
-            cout << field[j] << " - " << field[j + 1] << " ";
-        }
-        if (count == 0)
-            break;
-        cout << endl << MAX - i << "-й проход цикла завершен: ";
-        for (k = 0; k < MAX; k++) {
-            cout << field[k] << " ";
-        }
-        cout << endl;
-        count  = 0;
+    int source[MAX] = {1,2,3,4,5,6,7,8,23,4,234};
+    int target[MAX];
+    memcpy(target, source, sizeof(source));
+    for (int i =0; i< MAX; i++){
+        cout << target[i] << endl;
     }
 }
